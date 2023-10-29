4bc9db0aeb431ea20e03f56895ad0469be5b8b1b
sijuz <sijuz@yandex.ru>
2023-10-29 13:00:57 +0300

    s

--- src/logic/ton-api.ts
+++ src/logic/ton-api.ts
@@ -1,4 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { Blockchain } from './blockchain';
+import { Accounts } from './accounts';
+import { NFT } from './nft';
 
 export class TonApi {
   private _url: string = 'https://tonapi.io/v2/'
@@ -7,11 +10,20 @@ export class TonApi {
 
   private _network: 'testnet' | 'mainnet'
 
-  constructor (network: 'testnet' | 'mainnet') {
-    this._network = network
-  }
+  private _token: string
 
-  private _token: string = 'AFXRKLZM2YCJ67AAAAAE4XDRSACSYEOYKQKOSUVUKMXNMP2AKUTWJ2UVBPTTQZWRGZMLALY'
+  public Blockchain: Blockchain
+  public Accounts: Accounts
+  public NFT: NFT
+
+  constructor (token: string, network: 'testnet' | 'mainnet') {
+    this._network = network
+    this._token = token
+
+    this.Blockchain = new Blockchain(this)
+    this.Accounts = new Accounts(this)
+    this.NFT = new NFT(this)
+  }
 
   public async get (url: string, data: any): Promise<any | undefined> {
     let urlFull
